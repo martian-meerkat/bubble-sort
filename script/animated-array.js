b150ba0b62ca0257c0f1e9e94f7d0913597a926e
Meerkat <timon.miakyatto@gmail.com>
2018-07-06 02:32:55 +0100

    Comments.

--- script/animated-array.js
+++ script/animated-array.js
@@ -4,8 +4,8 @@
 
 function AnimatedArray(values) {
    this.values = values;
-   this.id = "";
-   this.slideDistance = "50px";
+   this.id = ""; // id tag for div containing an array.
+   this.slideDistance = "50px"; // The distance of slide animation.
 }
 
 AnimatedArray.prototype.setValues = function(values) {
@@ -16,7 +16,7 @@ AnimatedArray.prototype.drawArray = function(id) {
    this.id = id;
    html = document.createElement('div');
    html.id = id;
-   this.values.forEach(function(element) {
+   this.values.forEach(function(element) { // Appending an elements.
       var newElementDiv = document.createElement('div');
       var newElementSpan = document.createElement('span');
       newElementSpan.innerHTML = element;
@@ -33,9 +33,11 @@ AnimatedArray.prototype.swapAnimation = function(index) {
    var array = document.getElementById(this.id);
    var element = array.children[index];
    var nextElement = array.children[index+1];
+   // Swap values in an input array. 
    var temp = this.values[index];
    this.values[index] = this.values[index+1];
    this.values[index+1] = temp;
+   // Swap animation.
    $(element).animate({"left": '+=' + this.slideDistance}, 350);
    $(nextElement).animate({"left": '-=' + this.slideDistance}, 350, function() {
       $(element).css('left', '0px');
--- script/app.js
+++ script/app.js
@@ -10,6 +10,8 @@ function generateArray(length) {
    return arr;
 }
 
+var animationTimeout = 850; // The timeout between two animation steps.
+
 $(document).ready(function() {
    var timerId;
    $("#sort-button").on('click', function() {
@@ -20,23 +22,29 @@ $(document).ready(function() {
       }
       else
          $(".errortext").removeClass("visible");
-      clearTimeout(timerId);
+      clearTimeout(timerId); // Reset timer for animation.
       var values = generateArray(arrlen);
+
+      // Appending the original array.
       var inputArrayHtml = new animatedArray(values);
       if (!$("#input-array-container").is(":empty"))
          $("#input-array-container").html("");
       $("#input-array-container").append(inputArrayHtml.drawArray("input-array"));
+      // Appending the array for animation of swaps.
       var sortedArrayHtml = new animatedArray(values);
       if (!$("#sorted-array-container").is(":empty"))
          $("#sorted-array-container").html("");
       $("#sorted-array-container").append(sortedArrayHtml.drawArray("sorted-array"));
-      var sorted = sort.bubbleSort(values);
+
+      var sorted = sort.bubbleSort(values); // bubbleSort from animated-array.js
+      // We got list of swap steps in sorted.swapIndex.
       if (sorted.swapIndex.length > 0) {
          var index = 0;
+         // Recurrent setting of timeouts, due to index change.
          timerId = setTimeout(function swap() {
             sortedArrayHtml.swapAnimation(sorted.swapIndex[index]);
             if (index < sorted.swapIndex.length) {
-               timerId = setTimeout(swap, 850);
+               timerId = setTimeout(swap, animationTimeout);
                index++;
             }
          }, 610);
